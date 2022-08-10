94dbcd070c45f46bce18ce7884dd9c91614b56f3
Leonardo Keiser Fleck <89053412+Leonardo-KF@users.noreply.github.com>
2022-08-10 17:34:10 -0300

    feat: create services requisitions

---
+++ client/src/services/movie-api.ts
@@ -0,0 +1,14 @@
+const baseUrl = "http://localhost:3333/movies/";
+
+export const movieApi = {
+  getMovies: async (page: number) => {
+    const response = await fetch(baseUrl + "find/" + page);
+    const movies = await response.json();
+    return movies;
+  },
+  updateMovies: async () => {
+    const response = await fetch(baseUrl + "update");
+    const movies = await response.json();
+    return movies;
+  },
+};
---
+++ client/src/utils/types/movie.ts
@@ -0,0 +1,8 @@
+export type Movie = {
+  id: string;
+  title: string;
+  banner: string;
+  description: string;
+  director: string;
+  producer: string;
+};
